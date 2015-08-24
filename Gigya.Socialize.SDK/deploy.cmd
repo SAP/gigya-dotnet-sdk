c1e87f37be9dfe6e17d4780fbc74dce679659871
Dekel Barzilay <dekelev@gmail.com>
2015-08-24 18:32:13 +0300

    project re-organized and build fixed
    Commit by Dekel Barzilay <dekelev@gmail.com>
    On branch: refs/heads/feature/TP-2835

--- Gigya.Socialize.SDK/deploy.cmd
+++ Gigya.Socialize.SDK/deploy.cmd
@@ -3,22 +3,22 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC1\vcvarsall.bat"
 
 msbuild /p:Configuration=Release
 
-rmdir /S /Q ..\output
-mkdir ..\output
+rmdir /S /Q ..\Output
+mkdir ..\Output
 
-xcopy /Y ..\ProjectTemplate\* ..\output\GSCSharpSDK\
-xcopy /Y *.cs ..\output\GSCSharpSDK\
-xcopy /Y Properties\*.cs ..\output\GSCSharpSDK\Properties\
+xcopy /Y ..\ProjectTemplate\* ..\Output\GSCSharpSDK\
+xcopy /Y *.cs ..\Output\GSCSharpSDK\
+xcopy /Y Properties\*.cs ..\Output\GSCSharpSDK\Properties\
 
-msbuild ..\output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /target:Clean /p:Configuration=Release
-msbuild ..\output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\..\DO_NOT_PUBLISH_AssemblyStrongNameKey.snk
+msbuild ..\Output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /target:Clean /p:Configuration=Release
+msbuild ..\Output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\..\DO_NOT_PUBLISH_AssemblyStrongNameKey.snk
 
-msbuild ..\output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /target:Clean /p:Configuration=Debug
-msbuild ..\output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /p:Configuration=Debug   /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\..\DO_NOT_PUBLISH_AssemblyStrongNameKey.snk
+msbuild ..\Output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /target:Clean /p:Configuration=Debug
+msbuild ..\Output\GSCSharpSDK\Gigya.Socialize.SDK.csproj /p:Configuration=Debug   /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=..\..\DO_NOT_PUBLISH_AssemblyStrongNameKey.snk
 
-mkdir ..\output\bin
-move ..\output\GSCSharpSDK\bin\Release\*.dll ..\output\bin
-rmdir /S /Q ..\output\GSCSharpSDK\bin
-rmdir /S /Q ..\output\GSCSharpSDK\obj
+mkdir ..\Output\Bin
+move ..\Output\GSCSharpSDK\bin\Release\*.dll ..\Output\Bin
+rmdir /S /Q ..\Output\GSCSharpSDK\bin
+rmdir /S /Q ..\Output\GSCSharpSDK\obj
 
 pause
