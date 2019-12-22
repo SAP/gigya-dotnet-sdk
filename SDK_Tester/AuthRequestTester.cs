fdb5007f0f70fde4e2e89c683ffd95141e148dd3
kobi.co@gigya-inc.com <kobi.co@gigya-inc.com>
2019-12-22 13:12:57 +0200

    #98152 tester credentials and change log update

--- SDK_Tester/AuthRequestTester.cs
+++ SDK_Tester/AuthRequestTester.cs
@@ -5,39 +5,12 @@ namespace SDK_Tester
 {
     public class AuthRequestTester
     {
-        private const string ApiDomain = "il1-st12.gigya.com";
+        private const string ApiDomain = "il1.gigya.com";
         
-        private const string ApiKey = "3_iC1Ys3bom_XB1bKlO65ShCrOWFfHVC4FeI5uS9ra2WM_BXb6Uu-pOge2TtR6y_-1";
+        private const string ApiKey = "3_w8RBVwf9Sr_XtUbUrT3TRyhb59WOwEplCBGfjYdSAd0XduRZ_vdffS2mp7j_EsEz";
         private const string UserKey = "AMzUkXsKaA3Q";
 
-        private const string PrivateKey = @"-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAuYQMUHbzBHcgU9tDDwZElGvMrkyt6Rw56rdGL3yUHfvwUVn4
-L4HEoazaEJ9Wk9H0LVLmU1hoWagAI9tXACFFCk3+ycJfGX4ULDMz/5agXhrzZf0B
-dam8ZutJ1fxA3tRwd/xO8rypzeQU+wYNlvEJzAZT9JCASruG3Vq9n2hGMys+lS3f
-xtGmk0JBhE0RCz/TpJzMmx5WeB+3hhT/uukh23MGEHTtCK1DafaSxOSjtL0oeDlL
-hlB+HihEVaNmuXTWuQPc6STcuNIgzsrt0CEGVgHk1d+DH8p2CPwIf8FbpLZF3iiw
-4eZRWzrcQYfagwUbi49rprzQZp4//UAnR9jBPQIDAQABAoIBACyt2ohjiXZFGX5Q
-s5cR/6DOUJqW4ZifoWw/zRHBO2v4QiHZnP4WDw0QE+nGckPCIEBtM4cVpvYW0cfQ
-+uRPXvETJT6ixyQc2w9lGowfEwrvCzlAJKKZqUQRPTRh3x67g8XF+J6Z5PxvBsWJ
-KOs2LJGcYYpoZdl5zgqQINScOLH4OnzYTr4/pbuzoVKkWM+bZDFJoNtBBnG6+eHG
-k47sNkF4cQzW97MrxMwO4VDdhq/Kr5tK3ndJCrlTxrOGwPA2Nf3vqv43olIplaJf
-NZrCl1JKyIBmrvcWM4wxW+hGCrRDyOB2iFpg/XvgoimtBTYgPNoc7Nri7mJL+sc3
-Vke63vECgYEA4hETT+C/o/COysG7bv9bP8W6ABIN99kZNW4GcOddnz3GGz3EXS2q
-wgQyz8Ijk6ihuxVSU9yKb9Q0mT1fxTJW/4D5b5JxdN2nwsIbGhuK3sV2n6ThLbj/
-T6bBUlSAxogqimDCk/ERkNKnltD4JgKznlM2ENjc1pSbL9kVhxbC4rECgYEA0hRu
-ABs7CHTmcte9hNYebTvL6lYIOGV/Rk1DwW0ezE0Hw8p8jexVGSfHtH+qOJxGD6bc
-cfD9+beIaxfJVJ/9iGQiVdro3yfhPgeMQStcWAmijWBE/Z9xzutU9Os/fwMMpgQA
-65lqhcPsbG4CBiNmNq5eIbxj138uwqzV+UM0Mk0CgYEAjal9nJSOAsF/+Xalac0C
-9VeGUvz9W87jiSPFTYLunBctyWxPXMR9OM9AuAhEGweVMZMO4BZXefRUcaKQHRaK
-hdngdRYjmsQ7mEPij92qjCbZSvkbUneXJeatRlZFzCMP5V71D5gFFeertUqF9evD
-evdR7gS3fo/pH3a9ksWkokECgYBmfWsRCDfrr0SCgLhIJ0Ie3o5kW+aUxQer36QP
-qNHesDH6lj3f6420wRCQAbyk87DGkAx6Vi1B+AVI4gjqDUfek6OgqTT1MfqUjZAi
-dyoNFV5FhNMDvRcD8RG4j1CiAXXZRJjCWE18xxH/8EdygTCrurPX15YKG1VPyox1
-mBDN/QKBgAMUsxYhe4wnG4JjkMeb7kJNnr0jVdMgga3nwlw8QUb9C+oRpQIBZb9v
-+7TvXp92RiqPPP7fX5SoAGhgUHhLsiKFBdbrPghUmHbATm7EGFMtVK2QC4TlfVS0
-4o/LvlUlojOf6uFMViscfDS5YzcdjOx0xmOitIT1mo6haFDs4xx7
------END RSA PRIVATE KEY-----
-";
+        private const string PrivateKey = "-----BEGIN RSA PRIVATE KEY-----MIIEogIBAAKCAQEAo2FaiTqiq8Wdw3pUFiSoM8P6BIiI8hahOclT4mHdANZYWgiBK5YCQJMNo7BuhaxyabnZbLaHB1wbNHZP8UWmclbXdH/xAfveeOuVpul11HOVzrMxYl1LODTjbXF9jO36gWmpIJpTKlTDPNA38/pmyffsRHagfUGOQvnbvLzCFtYcVKzhH3/z8QSEo7JMJyzytFyrVKvDYtQMMZVgCw+9cI05gRJ+z6nI0/yzWzzEFnIRb7dI83QnibIoRsHidqQZUIDAK7cxHEGNkDTE6Eb6jditg40yU4BgvBrp7dTGdHtsW6EOA5JrQKbOJMsRcd9mPDkj0yRMwkBMj8grwH6PGQIDAQABAoIBAATa4kFctDPNigwiiPgle7gaFUZoNkWXZZAdivgZt2MMe5ClWw1MBmIb3JZmKkqfnsDEjJD99ZJC6u4KrAJ78t/H89wa5zMLZIeMXKSaoG1BSAzd51RIeHFBpRZ9/mCfO8f3t1ZoL5t87FZUy6zc3owW6Xb5XXiLZ6pW2XBI3a2m//OzJTcCTpNun75sj1chtmiCqpqJnwegPvn1LIeSC79FuNIwja+WI5rDHBaWBmzZsvDhx3oxzsSmBIQAZ2AehxWB4pIIz4Qmx3cYy5D82bDb2kErq8eNli1oLpdUqy/PkVt3i8XtGxpiQXUVaD6+rAFK8wbxKeUJdoR9bzCLcT8CgYEA49wllCOdcAdJSgZASnL+lGEyd+vHb5jSbOwnJ7rtozClEuZ9KOxyGEgmJDrZ2Q6ITKiSCsajy0b8zY3oyglWcRKkhLaIFjAeE8tz9HmfrDanbzBRCB9ZLSvYlZcvSHVomzutqaXs7qsFZ0Mt9eZcNBFKqE4z6tz8bQoy6t6XLDMCgYEAt46qRx00ZdhahiCHAQV58oGOnTNcqVS3VtCuw42CviBtsTrnYIREex/jl6Ui90WtuSJHC06f/dGwZ0Xns90/ChocG2T3KpHstg4RgN4RoEXE+qADDl9aLIggrahPD6V021+V5i1J3JxJjEzUVt/6+vaN9KpPxXFpADGG1gQQS4MCgYA7UkceSB9m2R7FfNckCsgojR18hw/HB/xQizKub0YK5FE1mHghPV1+4Nm9OO0aS2REwOY0k/50n6iVQ0rFvqSYj4fxXSwUyrYp5R/tF/Tv+tKgae3OtYqb7fxXBaMztA1lzKWrsxz6DeA8QAspJ639iDrtkl6F2L6HDM6wwv6MbQKBgDonRcUv+HjHua5CweLN9FujNiaRriqrf0ZO6P9lZuWLapU6vzEx1mxXpwhVNiW2+pnrxSxM5Z1JgKTHXef7EUzHBt6a9z+Sabcn792u/VCUqhpo9W7pQK1ZF1lNOHcRiVszBk+dS4hML3T2plM7tM0rrb+08X7xNj3scvZ85Ri3AoGASPNY8AL/matu6V5RT4ccjYKTySjnmz806OReR7IVhBD9xf+08DsAEOkhL9e9Tj6f7ENhwRWT+7EY+eSg5zFZv5ATIU+y7FNLJHockG7bgwM6H2oJptI1kPHmiMCpnjP9kKAb8C8h223dl7v1ChvWu6/oXSZ+gDuwN+8F4V7bksU=-----END RSA PRIVATE KEY-----";
 
         public void Run()
         {
@@ -49,7 +22,7 @@ mBDN/QKBgAMUsxYhe4wnG4JjkMeb7kJNnr0jVdMgga3nwlw8QUb9C+oRpQIBZb9v
                 method,
                 new
                 {
-                    uid = "100c3d9b00c54c519ebd9afebd31c6f4", enabledProviders = "*,testnetwork,testnetwork2"
+                    uid = "654916f7a76f4980b78e575a6fc9f470", enabledProviders = "*,testnetwork,testnetwork2"
                 })
             {
                 APIDomain = ApiDomain
