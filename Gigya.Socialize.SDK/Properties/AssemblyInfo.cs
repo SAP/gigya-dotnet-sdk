06dc785f4c6b691ec22d515ef46b1ed24b4ab8bc
kobi.cohen@sap.com <kobi.cohen@sap.com>
2020-04-16 14:30:00 +0300

    #111188 upgrade version

--- Gigya.Socialize.SDK/Properties/AssemblyInfo.cs
+++ Gigya.Socialize.SDK/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 
-[assembly: AssemblyVersion("2.16.5.0")]
-[assembly: AssemblyFileVersion("2.16.5.0")]
+[assembly: AssemblyVersion("2.16.6.0")]
+[assembly: AssemblyFileVersion("2.16.6.0")]
